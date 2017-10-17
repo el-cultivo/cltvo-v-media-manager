6f25090c3b9bd81e866ab6d1ea30d418c58f3614
Diego <diego.vid.eco@gmail.com>
2017-10-17 15:58:09 -0500

    agrega transformacion de babel para coral-std-library

--- karma.conf.js
+++ karma.conf.js
@@ -45,7 +45,7 @@ module.exports = function(config) {
                 rules: [
                     {
                         test: /\.js$/,
-                        exclude: /(node_modules|bower_components)/,
+                        exclude: /node_modules\/(?!(coral-std-library)\/).*/,
                         use: {
                             loader: 'babel-loader?cacheDirectory',
                             options: {
--- webpack.config.js
+++ webpack.config.js
@@ -51,7 +51,7 @@ module.exports = {
 		rules: [
 			{
 				test: /\.js$/,
-				exclude: /(node_modules|bower_components)/,
+				exclude: /node_modules\/(?!(coral-std-library)\/).*/,
 				use: {
 					loader: 'babel-loader?cacheDirectory',
 					options: {
